39ac306e5e217f36adfc67fa289281070d54912e
José de Jesús Acevedoe <jose.de.jesus.acevedo.flores@gmail.com>
2022-10-31 22:13:54 -0600

    Final

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -5,24 +5,22 @@ import useDataRequest from '../Hooks/useDataRequest';
 
 const Weather = () => {
 
-    const { dataWeather } = useDataRequest(    
-    ) 
+    const { dataWeather } = useDataRequest(
+    )
 
-    const [change, setChange] =useState(true)
+    const [change, setChange] = useState(true)
 
     const shuffle = () => {
         setChange(!change)
     }
-    
-        console.log(dataWeather?.main?.feels_like)
-        console.log(dataWeather)
-        const nube = dataWeather.weather?.[0]?.icon
+
+    const icon = dataWeather.weather?.[0]?.icon
     return (
         <div className='Weather'>
             <h1>{dataWeather?.sys?.country}</h1>
             <h2>{dataWeather.name}</h2>
-            <img src={`http://openweathermap.org/img/wn/${nube === undefined ? {} : nube }@4x.png`} alt="" />
-           {change === true ? <h1>{dataWeather?.main?.feels_like}°K</h1>: <h1>{dataWeather?.main?.feels_like - 273.15}°C</h1> } 
+            {icon && <img src={`http://openweathermap.org/img/wn/${icon}@4x.png`} alt="" />}
+            {change === true ? <h1>{dataWeather?.main?.feels_like}°K</h1> : <h1>{dataWeather?.main?.feels_like - 273.15}°C</h1>}
             <button onClick={shuffle}>Change °K / °C</button>
         </div>
     );
